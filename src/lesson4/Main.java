875633aa7b72b6a7748633f10597095872b9c73d
psiholirik <aarubcov0822@gmail.com>
2019-01-28 17:01:17 +0300

    поменял textArea на textField

--- src/lesson4/Main.java
+++ src/lesson4/Main.java
@@ -6,13 +6,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 public class Main extends Application {
   public Button sendButton;
-  public TextArea message;
+  public TextField message;
   public TextArea chat;
 
   @Override
