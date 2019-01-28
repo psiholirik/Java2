ba9762f11d75704e46c278281d9b384002ad2b72
psiholirik <aarubcov0822@gmail.com>
2019-01-28 09:09:09 +0300

    lesson4

---
+++ src/lesson4/Main.java
@@ -0,0 +1,46 @@
+package lesson4;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+  public Button sendButton;
+  public TextArea message;
+  public TextArea chat;
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    Parent parent = FXMLLoader.load(getClass().getResource("main.fxml"));
+
+    primaryStage.setTitle("Чат");
+    primaryStage.setScene(new Scene(parent));
+    primaryStage.show();
+  }
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+  public void sendToChatByEnter(KeyEvent keyEvent) {
+    if (keyEvent.getCode() == KeyCode.ENTER && message.getText().length() != 0) {
+      setTextToChat(message.getText());
+    }
+  }
+
+  public void sendToChatByButton() {
+    if(message.getText().length() != 0) {
+      setTextToChat(message.getText());
+    }
+  }
+
+  private void setTextToChat(String s) {
+    message.clear();
+    chat.setText(chat.getText() + "Artem: " + s + "\n");
+  }
+}
